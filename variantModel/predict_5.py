7cd6da2019e0e1d054cbdb2f8514ab8855aab8ae
CSUBioGroup <limin@csu.edu.cn>
2022-11-02 16:28:05 +0800

    Update predict_5.py

--- variantModel/predict_5.py
+++ variantModel/predict_5.py
@@ -4,7 +4,7 @@ mapLocation = {'cuda:0': 'cpu', 'cuda:1': 'cpu'}
 GraphLncLoc = []
 for i in range(1, 6):
     GraphLncLoc.append(
-        lncRNALocalizer(weightPath=f"checkpoints/Final_model/fold{i}_5.pkl", n_classes=5, map_location=mapLocation))
+        lncRNALocalizer(weightPath=f"Final_model/fold{i}_5.pkl", n_classes=5, map_location=mapLocation))
 
 
 def lncRNA_loc_predict(lncRNA):
